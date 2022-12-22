aa453736feace5f324e5175e59c6044c8e5d4b24
Elmer Jacobo Otiniano <88252912+elmerjacobo97@users.noreply.github.com>
2022-12-22 12:14:04 -0500

    updated production

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   ignoredRouteFiles: ["**/.*"],
+  future: {},
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
